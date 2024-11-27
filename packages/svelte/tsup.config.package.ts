d5bb8720575cd6dabfa2898220a2582cebb16529
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-11-26 22:01:41 -0500

    feat: scaffold svelte

---
+++ packages/svelte/tsup.config.package.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "tsup";
+
+import { getBaseConfig } from "../build-scripts/getBaseConfig";
+
+export default defineConfig((options = {}) => [
+  ...getBaseConfig("node", ["cjs", "esm"], options),
+  ...getBaseConfig("browser", ["cjs", "esm"], options),
+]);

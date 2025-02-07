52f1854bf91b7135a23cba2af1f1bda9179aa1fb
Nick Frostbutter <75431177+nickfrosty@users.noreply.github.com>
2025-02-07 13:55:39 -0500

    feat: add examples (#14)
    * feat: add examples
    
    * ci: tests
    
    * feat: token clients
    
    * chore: programs
    
    * feat: full e2e example

---
+++ examples/esrun/src/index.ts
@@ -0,0 +1,99 @@
+import {
+  getExplorerLink,
+  createTransaction,
+  createSolanaClient,
+  SolanaClusterMoniker,
+  getSignatureFromTransaction,
+  sendAndConfirmTransactionFactory,
+  signTransactionMessageWithSigners,
+} from "gill";
+import { loadKeypairSignerFromFile } from "gill/node";
+import { getAddMemoInstruction } from "gill/programs";
+
+/**
+ * Load a keypair signer from the local filesystem
+ *
+ * This defaults to the file path used by the Solana CLI: `~/.config/solana/id.json`
+ */
+const signer = await loadKeypairSignerFromFile();
+console.log("address:", signer.address);
+
+/**
+ * Declare what Solana network cluster we want our code to interact with
+ */
+const cluster: SolanaClusterMoniker = "devnet";
+
+/**
+ * Create a client connection to the Solana blockchain
+ *
+ * Note: `urlOrMoniker` can be either a Solana network moniker or a full URL of your RPC provider
+ */
+const { rpc, rpcSubscriptions } = createSolanaClient({
+  urlOrMoniker: cluster,
+});
+
+/**
+ * Initialize the transaction sender function with our RPC
+ */
+const sendAndConfirmTransaction = sendAndConfirmTransactionFactory({
+  rpc,
+  rpcSubscriptions,
+});
+
+/**
+ * Create a memo instruction to post a simple message onchain
+ * (the simplest of instruction types!)
+ */
+const memoIx = getAddMemoInstruction({
+  memo: "gm world!",
+});
+
+/**
+ * Get the latest blockhash (aka transaction lifetime). This acts as a recent timestamp
+ * for the blockchain to key on when processing your transaction
+ *
+ * Pro tip: only request this value just before you are going to use it your code
+ */
+const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
+console.log("latestBlockhash:", latestBlockhash);
+
+/**
+ * Create a transaction to be sent to the blockchain
+ */
+let tx = createTransaction({
+  version: "legacy",
+  feePayer: signer,
+  instructions: [memoIx],
+  latestBlockhash,
+});
+console.log(tx);
+
+/**
+ * Sign the transaction with the provided `signer` when it was created
+ */
+let signedTransaction = await signTransactionMessageWithSigners(tx);
+console.log("signedTransaction");
+console.log(signedTransaction);
+
+/**
+ * Log the Solana Explorer link for the
+ */
+console.log(
+  getExplorerLink({
+    cluster,
+    transaction: getSignatureFromTransaction(signedTransaction),
+  }),
+);
+
+try {
+  /**
+   * Actually send the transaction to the blockchain and confirm it
+   */
+  await sendAndConfirmTransaction(signedTransaction, {
+    commitment: "confirmed",
+  });
+  console.log("Transaction confirmed!");
+} catch (err) {
+  console.error("Unable to send and confirm the transaction");
+  console.error(err);
+}
